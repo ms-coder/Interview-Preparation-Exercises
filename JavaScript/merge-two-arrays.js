4c491f51a013ba6a4c17d5e224307561eadeb855
ms-coder <igor.tuhh@gmail.com>
2019-06-20 00:38:10 +0200

    first test commit (the code was not reviewed or tested)

---
+++ JavaScript/merge-two-arrays.js
@@ -0,0 +1,21 @@
+function mergeArray(arr1, arr2) {
+    var new_array = [];
+    var i = 0,
+        j = 0,
+        index = 0;
+   
+    while (new_array.length != (arr1.length + arr2.length) - 1) {
+        if (arr1[i] < arr2[j]) {
+            new_array.push(arr1[i]);
+            i++;
+        } else {
+            new_array.push(arr2[j]);
+            j++;
+        }
+    }
+    return new_array;
+}
+
+let a = mergeArray([3,4,1,7], [2,6,9,0]);
+
+console.log(a.join());
